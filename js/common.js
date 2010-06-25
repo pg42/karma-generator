df9c93399c9e572941545965535e8229b4f9686d
Peter <Peter.Gijsels@gmail.com>
2010-06-25 15:58:25 +0545

    Moved setUpMultiScreenLesson to common.js

--- js/common.js
+++ js/common.js
@@ -1,63 +1,63 @@
 // This file can replace global.js in karma.
 
+
+function  controlButtonClickCallback($button, callback) {
+    $button.click(function () {
+                      if (!$button.data('disabled')) {
+                          callback();
+                      }
+                  });
+}
+
+function enableControlButton($button) {
+    $button.css({opacity: 1, cursor: 'pointer'});
+    $button.data('disabled', false);
+}
+
+function disableControlButton($button) {
+    $button.css({opacity: 0.3, cursor: 'default'});
+    $button.data('disabled', true);
+}
+
+function setUpLinkBackLesson() {
+    $('#linkBackLesson')
+        .click(function () {
+                   window.location = '../../../MenuStage.html';
+               });
+}
+
+function setUpStartAndPlayAgainButtons(karma, initialize, start_game) {
+    // Why two buttons?
+    // Why not:
+    // * only a play button?
+    // -or-
+    // * a button that is initially play, later replay?
+    disableControlButton($('#linkPlayAgain'));
+    controlButtonClickCallback($('#linkStart'),
+                               function () {
+                                   initialize(karma);
+                                   disableControlButton($('#linkStart'));
+                                   enableControlButton($('#linkPlayAgain'));
+                                   start_game(karma);
+                               });
+    controlButtonClickCallback($('#linkPlayAgain'),
+                               function () { start_game(karma); });
+}
+
+function setUpHelp() {
+    $help = $('#help');
+    $('#linkHelp')
+        .click(function () { $help.slideDown(2000); })
+        .mouseout(function () { $help.slideUp(2000); });
+}
+
 /*
  * initialize is called once before the game is started. It is passed
  * the karma object as an argument.
  * Each time a game is started or restarted, starte_game is called
  * with the karma object as an argument.
  */
-function setUpLesson(initialize, start_game) {
-
-    var controlButtonClickCallback = function ($button, callback) {
-        $button.click(function () {
-                          if (!$button.data('disabled')) {
-                              callback();
-                          }
-                      });
-    };
-
-    var enableControlButton = function ($button) {
-        $button.css({opacity: 1, cursor: 'pointer'});
-        $button.data('disabled', false);
-    };
-
-    var disableControlButton = function ($button) {
-        $button.css({opacity: 0.3, cursor: 'default'});
-        $button.data('disabled', true);
-    };
-
-    var setUpLinkBackLesson = function () {
-        $('#linkBackLesson')
-            .click(function () {
-                       window.location = '../../../MenuStage.html';
-                   });
-    };
-
-    var setUpStartAndPlayAgainButtons = function (karma, initialize, start_game) {
-        // Why two buttons?
-        // Why not:
-        // * only a play button?
-        // -or-
-        // * a button that is initially play, later replay?
-        disableControlButton($('#linkPlayAgain'));
-        controlButtonClickCallback($('#linkStart'),
-                                   function () {
-                                       initialize(karma);
-                                       disableControlButton($('#linkStart'));
-                                       enableControlButton($('#linkPlayAgain'));
-                                       start_game(karma);
-                                   });
-        controlButtonClickCallback($('#linkPlayAgain'),
-                                   function () { start_game(karma); });
-    };
-
-    var setUpHelp = function () {
-        $help = $('#help');
-        $('#linkHelp')
-            .click(function () { $help.slideDown(2000); })
-            .mouseout(function () { $help.slideUp(2000); });
-    };
-
+function setUpLesson(draw_screen_fns, start_game) {
     $(function () {
           var karma = lesson_karma();
           karma.ready(
@@ -67,4 +67,72 @@ function setUpLesson(initialize, start_game) {
                   setUpHelp();
               });
       });
+}
+
+function setUpMultiScreenLesson(draw_screen_fns) {
+    $(function () {
+          $('#linkPrevLesson').hide();
+          $('#linkNextLesson').hide();
+
+          var karma = lesson_karma();
+          var $content = $('#content'); // TBD: error if absent
+          var current_screen = 0;
+
+          var gotoScreen = function(i) {
+              current_screen = i;
+              updateScreen();
+          };
+
+          var updateScreen = function () {
+              $content.empty();
+              draw_screen_fns[current_screen](karma, $content);
+              if (current_screen == 0) {
+                  $('#linkPrevLesson').hide();
+              } else {
+                  $('#linkPrevLesson').show();
+              }
+              if (current_screen == draw_screen_fns.length - 1) {
+                  $('#linkNextLesson').hide();
+              } else {
+                  $('#linkNextLesson').show();
+              }
+          };
+
+          $('#linkNextLesson')
+              .click(function () {
+                         karma.audio.trigger.play();
+                         ++current_screen;
+                         updateScreen();
+                     });
+          $('#linkPrevLesson')
+              .click(function () {
+                         karma.audio.trigger.play();
+                         --current_screen;
+                         updateScreen();
+                     });
+
+          karma.ready(
+              function () {
+                  setUpLinkBackLesson();
+                  setUpStartAndPlayAgainButtons(karma,
+                                                function () {},
+                                                function (karma) {
+                                                    gotoScreen(0);
+                                                });
+                  setUpHelp();
+              });
+      });
+}
+
+function disableSelection(target) {
+    if (typeof target.onselectstart != 'undefined') { //IE
+        target.onselectstart = function () { return false; };
+    } else if (typeof target.style.MozUserSelect != 'undefined') { //Firefox
+        target.style.MozUserSelect = 'none';
+    } else if (typeof target.style.KhtmlUserSelect != 'undefined') { //Webkit
+        target.style.KhtmlUserSelect = 'none';
+    } else { //Opera
+        target.onmousedown = function () { return false; };
+        target.style.cursor = 'default';
+    }
 }
--- templates/vocabulary/vocabulary.js
+++ templates/vocabulary/vocabulary.js
@@ -1,65 +1,4 @@
-var objTxtHeight = 50;
-
-function createImage(object) {
-    var img_area = $(document.createElement('div'))
-        .addClass('imgArea');
-    // TBD: is this div needed?
-    var imgObject = $(document.createElement('div'))
-        .addClass('imgObject')
-        .appendTo(img_area);
-    $(document.createElement('img'))
-        .attr('src', k.image[object.name].src)
-        .mousedown(function (event) { event.preventDefault(); }) // No dragging
-        .appendTo(imgObject);
-    $(document.createElement('div'))
-        .addClass('dropObjects')
-        .appendTo(img_area);
-    return img_area;
-}
-
-function createWord(object) {
-    return $(document.createElement('div'))
-        .addClass('dragObjects')
-        .html(object.name);
-}
-
-var gotoScreen = function (i) { alert('call setUpMultiScreen first!'); };
-
-function setUpMultiScreen($container, screen_generators) {
-    var current_screen = 0;
-    gotoScreen = function(i) {
-        current_screen = i;
-        updateScreen();
-    };
-    var updateScreen = function () {
-        $container.empty();
-        screen_generators[current_screen]($container);
-        if (current_screen == 0) {
-            $('#linkPrevLesson').hide();
-        } else {
-            $('#linkPrevLesson').show();
-        }
-        if (current_screen == screen_generators.length - 1) {
-            $('#linkNextLesson').hide();
-        } else {
-            $('#linkNextLesson').show();
-        }
-    };
-    $('#linkNextLesson')
-        .click(function () {
-                   k.audio.trigger.play();
-                   ++current_screen;
-                   updateScreen();
-               });
-    $('#linkPrevLesson')
-        .click(function () {
-                   k.audio.trigger.play();
-                   --current_screen;
-                   updateScreen();
-               });    
-}
-
-function generateScreen0($container) {
+function generateScreen0(karma, $container) {
     var $vocab_img = $(document.createElement('div'))
         .attr('id', 'vocabImg')
         .appendTo($container);
@@ -67,45 +6,70 @@ function generateScreen0($container) {
     var object_css = function(object) {
         var result = object.position;
         result.position = 'absolute';
-        result.height = objTxtHeight;
+        result.height = 50;
         return result;
     };
 
     $(objects.map(function (object) {
                       return $(document.createElement('div'))
                           .css(object_css(object))
-                          .click(function () { k.audio[object.name].play(); });
+                          .click(function () {
+                                     karma.audio[object.name].play();
+                                 });
                   })).appendTo($vocab_img);
 }
 
-function enable_drag_and_drop(word, target) {
-    word.draggable({containment: '#content', revert: true});
-    target.droppable(
-        {tolerance: 'intersect',
-         hoverClass: 'drophover',
-         drop: function (event, ui) {
-             var dropped_word = ui.draggable;
-             if (dropped_word.get()[0] === word.get()[0]) {
-                 dropped_word.remove();
-                 $(this).text(dropped_word.text());
-                 k.audio.correct.play();
-             } else {
-                 k.audio.incorrect.play();
-             }
-             if ($('.dragObjects').size() == 0) {
-                 $(document.createElement('div'))
-                     .addClass('gameOver')
-                     .text('GAME OVER')
-                     .appendTo($('#optionSection'));
-             }
-         }});
-}
+function generateScreen1(karma, $container) {
+    var createImage = function (object) {
+        var img_area = $(document.createElement('div'))
+            .addClass('imgArea');
+        // TBD: is this div needed?
+        var imgObject = $(document.createElement('div'))
+            .addClass('imgObject')
+            .appendTo(img_area);
+        $(document.createElement('img'))
+            .attr('src', karma.image[object.name].src)
+            .mousedown(function (event) {
+                           event.preventDefault(); // No dragging
+                       })
+            .appendTo(imgObject);
+        $(document.createElement('div'))
+            .addClass('dropObjects')
+            .appendTo(img_area);
+        return img_area;
+    };
 
-function generateScreen1($container) {
-    correctCounter = 0;
+    var createWord = function (object) {
+        return $(document.createElement('div'))
+            .addClass('dragObjects')
+            .html(object.name);
+    };
 
     var images = objects.map(createImage);
     var words = objects.map(createWord);
+
+    var enable_drag_and_drop = function (word, target) {
+        word.draggable({containment: '#content', revert: true});
+        target.droppable(
+            {tolerance: 'intersect',
+             hoverClass: 'drophover',
+             drop: function (event, ui) {
+                 var dropped_word = ui.draggable;
+                 if (dropped_word.get()[0] === word.get()[0]) {
+                     dropped_word.remove();
+                     $(this).text(dropped_word.text());
+                     karma.audio.correct.play();
+                 } else {
+                     karma.audio.incorrect.play();
+                 }
+                 if ($('.dragObjects').size() == 0) {
+                     $(document.createElement('div'))
+                         .addClass('gameOver')
+                         .text('GAME OVER')
+                         .appendTo($('#optionSection'));
+                 }
+             }});
+    };
 
     for (var i = 0; i < images.length; ++i) {
         enable_drag_and_drop(words[i], $('.dropObjects', images[i]));
@@ -116,24 +80,12 @@ function generateScreen1($container) {
         .appendTo($container);
     $(Karma.shuffle(images)).appendTo($ques_section);
 
+    disableSelection($ques_section.get()[0]);
+
     var $option_section = $(document.createElement('div'))
         .attr({id: 'optionSection'})
         .appendTo($container);
     $(Karma.shuffle(words)).appendTo($option_section);
 }
 
-function game() {
-    gotoScreen(0);
-}
-
-$(function () {
-      $('#linkPrevLesson').hide();
-      $('#linkNextLesson').hide();
-});
-
-function initialize(karma) {
-    k = karma;
-    setUpMultiScreen($('#content'), [generateScreen0, generateScreen1]);
-}
-
-setUpLesson(initialize, game);
+setUpMultiScreenLesson([generateScreen0, generateScreen1]);
