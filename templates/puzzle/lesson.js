d66bd1c4d7da913f8b1b7c03542eb82a1331f7ca
Peter <Peter.Gijsels@gmail.com>
2010-06-25 16:31:35 +0545

    Disabled default selection behaviour in puzzle.

--- templates/puzzle/lesson.js
+++ templates/puzzle/lesson.js
@@ -151,9 +151,10 @@ function createThumbnail(karma, puzzle) {
 
 function initialize(karma) {
     $imageBar = $(document.createElement('div')).attr({id: 'imageBar'});
-    $('#content')
+    $content = $('#content')
         .append($imageBar)
         .append($(document.createElement('div')).attr({id: 'imgMain'}));
+    disableSelection($content.get()[0]);
     $(puzzles.map(function (puzzle) { return createThumbnail(karma, puzzle); }))
         .appendTo($imageBar);
 }
