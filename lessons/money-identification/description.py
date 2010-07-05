88434d493a877de9c31807254827ff047e1b9a4e
Peter <Peter.Gijsels@gmail.com>
2010-07-05 15:34:29 +0545

    Added money-identification lesson.

---
+++ lessons/money-identification/description.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+lesson(grade=2, subject='Maths', title='Money Identification', week=18,
+       nepalese_title=u'रुपियाँको चिनारी')
+
+css('global')
+
+for f in ['ui.core', 'karma', '../../js/common.js']:
+    java_script(f)
+
+for i in [1, 2, 5, 10, 20, 25, 50, 100, 500, 1000]:
+    image('Rs%s.png' % i, i)
+    audio('Rs%s.wav' % i, i)
+
+image('correct.png', 'correct')
+image('incorrect.png', 'incorrect')
+
+image('optionBorder.png')
+image('optionBorderHover.png')
+image('listenAgain.png')
+
+audio('ne_correct.wav', 'correct')
+audio('ne_incorrect.wav', 'incorrect')
+audio('byebye.wav', 'byebye')
+
+css('lesson.css')
+java_script('lesson.js')
+
+div(id='content')
+
+footer_configuration(link_next=False, link_previous=False, scoreboard=True)
