b576e1ab05380399c5894154fad0e200f9f3b5e6
Peter <Peter.Gijsels@gmail.com>
2010-07-14 17:32:56 +0545

    Added lesson number-words.

---
+++ lessons/number-words/description.py
@@ -0,0 +1,19 @@
+lesson(grade=2, subject='English', title= 'Number Words', week=14)
+
+css('global')
+css('lesson.css')
+
+for f in ['ui.core',
+          'karma',
+          '../../js/common.js',
+          'lesson.js']:
+    java_script(f)
+
+audio('en_correct.ogg', 'correct')
+audio('en_incorrect.ogg', 'incorrect')
+
+image('football.png', 'football')
+image('gameOver.png', 'gameOver')
+
+div(id='content')
+
