cd800b256f97f5bcd3dc3a1e84ffa450db217963
Peter <Peter.Gijsels@gmail.com>
2010-07-15 14:12:21 +0545

    Added adding-numbers lesson.

---
+++ lessons/adding-numbers/description.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+lesson(grade=2, subject='Maths', title='Adding Numbers', week=4,
+       lesson_title=u'वस्तुहरूको जोड')
+
+
+css('global')
+css('lesson.css')
+
+for f in ['ui.core',
+          'karma',
+          '../../js/common.js',
+          'lesson.js']:
+    java_script(f)
+
+div(id='content')
+
+audio('byebye.ogg', 'byebye')
+audio('ne_correct.wav', 'correct')
+audio('ne_incorrect.wav', 'incorrect')
+
+image('background.jpg')
+
+footer_configuration(link_next=False, link_previous=False, scoreboard=True)
