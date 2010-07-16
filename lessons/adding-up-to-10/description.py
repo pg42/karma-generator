46c3a9b5f1d2b7f633472f23b72de27a7d70b1a9
Peter <Peter.Gijsels@gmail.com>
2010-07-16 16:35:59 +0545

    Added lesson adding-up-to-10

---
+++ lessons/adding-up-to-10/description.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+lesson(grade=2, subject='Maths', title='Adding up to 10 Fun Game', week=3,
+       browser_title='Adding up to 10',
+       lesson_title=u'दससम्मको जोड')
+
+css('global')
+css('lesson.css')
+
+for f in ['ui.core',
+          'karma',
+          '../../js/common.js',
+          'lesson.js']:
+    java_script(f)
+
+div(id='content')
+
+audio('correct.ogg', 'correct')
+audio('incorrect.ogg', 'incorrect')
+audio('trigger.ogg', 'trigger')
+
+image('yellowBox.png', 'yellowBox')
+image('plussign.png', 'plussign')
+image('scorebox.png', 'scorebox')
+
+image('ball37px.png', 'ball')
+image('balloon37px.png', 'balloon')
+image('banana37px.png', 'banana')
+image('chilli.png', 'chilli')
+image('fish64px.png', 'fish')
+image('flower37px.png', 'flower')
+image('normalChimp_120x125.png', 'normal_chimp')
+image('happyChimp_120x125.png', 'happy_chimp')
+image('sadChimp_120x125.png', 'sad_chimp')
+
+
+footer_configuration(link_next=False, link_previous=False, scoreboard=True)
