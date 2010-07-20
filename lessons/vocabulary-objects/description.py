08b01393f04869ce88c1b6a19fd4d2ac2cd67bb7
Bidhan Adhikari <bidhan.adhikari@olenepal.org>
2010-07-19 21:41:46 -0700

    vocabulary-objects small fixes

--- lessons/vocabulary-objects/description.py
+++ lessons/vocabulary-objects/description.py
@@ -1,8 +1,7 @@
 lesson(grade=2, subject='English', title='Vocabulary Objects', week=5)
 
 include('../../templates/vocabulary/description.py')
-
-animals = [
+objects = [
     dict(name='bag', left=650, top=287, width=110),
     dict(name='bench', left=622, top=553, width=154),
     dict(name='book', left=906, top=287, width=110),
@@ -10,8 +9,8 @@ animals = [
     dict(name='desk', left=385, top=553, width=110),
     dict(name='kite', left=130, top=553, width=105),
     dict(name='ladder', left=890, top=553, width=150),
-    dict(name='soap', left=379, top=650, width=120)]
+    dict(name='soap', left=379, top=287, width=120)]
 
-register_objects(animals)
+register_objects(objects)
 
 image('vocabImg.jpg')
