3702d81aa527ead70741d314d2fa4c603446222c
Peter <Peter.Gijsels@gmail.com>
2010-07-21 12:17:03 +0545

    Corrected lesson info in months-of-the-year description.

--- lessons/months-of-the-year/description.py
+++ lessons/months-of-the-year/description.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-lesson(grade=6, subject='English', title='Months of the Year', week=3,
-       browser_title='Months of the Year',
-       lesson_title=u'Months of the Year')
+lesson(grade=6, subject='English', title='Months', week=1,
+       browser_title='Class 6 English Months',
+       lesson_title='Months of the Year')
 
 css('global')
 css('lesson.css')
@@ -13,28 +13,28 @@ for f in [
         '../../js/common.js',
         'lesson.js'
 	]:
-	java_script(f)
+        java_script(f)
 
 div(id='content')
 
 months = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December'
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
         ]
 for month in months:
-	image(month + '.png', month)
-	image('small_' + month + '.png', 'small_' + month)
-	audio(month + '.ogg', month)
+        image(month + '.png', month)
+        image('small_' + month + '.png', 'small_' + month)
+        audio(month + '.ogg', month)
 
 audio('correct.ogg', 'correct')
 audio('incorrect.ogg', 'incorrect')
