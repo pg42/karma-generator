7f060c8a8d7dd5e0024f5ba143632d3c3175e882
sudaya <sudaya@olenepal.org>
2010-07-22 10:20:47 +0545

    Added vocabulary-plants lesson.

---
+++ lessons/vocabulary-plants/description.py
@@ -0,0 +1,17 @@
+lesson(grade=2, subject='English', title='Vocabulary Plant', week=17)
+
+include('../../templates/vocabulary/description.py')
+
+animals = [
+    dict(name='leaf', left=175, top=69, width=110),
+    dict(name='branch', left=953, top=130, width=160),
+    dict(name='fruit', left=132, top=190, width=115),
+    dict(name='tree', left=470, top=217, width=110),
+    dict(name='bush', left=169, top=340, width=125),
+    dict(name='flower', left=865, top=340, width=145),
+    dict(name='grass', left=260, top=605, width=140),
+    dict(name='plant', left=580, top=585, width=140)]
+
+register_objects(animals)
+
+image('vocabImg.jpg')
