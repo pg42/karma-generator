6e26c60af14c68f7fecf9c1fb8fe480855fe05d3
Peter <Peter.Gijsels@gmail.com>
2010-07-27 15:04:29 +0545

    Added vocabulary-animals lesson.

---
+++ lessons/vocabulary-animals/description.py
@@ -0,0 +1,41 @@
+lesson(grade=6, subject='English', title='Vocabulary Animals', week=4,
+       lesson_title='Vocabulary: Animals')
+
+animals = ['cheetah',
+           'crocodile',
+           'dinosaur',
+           'giraffe',
+           'kangaroo',
+           'octopus',
+           'panda',
+           'rhinoceros',
+           'whale',
+           'zebra']
+
+css('global')
+css('lesson.css')
+
+for f in ['ui.core',
+          'ui.draggable',
+          'ui.droppable',
+          'karma',
+          '../../js/common.js',
+          '../../js/jquery.clickable.js']:
+    java_script(f)
+
+objects_js = java_script('objects.js', generated=True)
+
+print >>objects_js, 'var objects = ['
+print >>objects_js, '   ' + ',\n   '.join(["'%s'" % x for x in animals])
+print >>objects_js, '];'
+
+java_script('lesson.js')
+
+for animal in animals:
+    image(animal + '.png', animal)
+    audio(animal + '.ogg', animal)
+
+audio('correct.ogg', 'correct')
+audio('incorrect.ogg', 'incorrect')
+
+image('volImage.png')
