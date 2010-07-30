34a18bd9096ee4c827a26366cdd6c720f55d10b4
Peter <Peter.Gijsels@gmail.com>
2010-07-30 12:07:33 +0545

    Added estimating-angles lesson.

---
+++ lessons/estimating-angles/description.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+lesson(grade=6, subject='Maths', title='Estimating Angles', week=16,
+       lesson_title=u'कोणको माप')
+
+css('global')
+css('lesson.css')
+
+for f in ['lesson.js']:
+    java_script(f)
+
+for x in ['correct', 'incorrect']:
+    audio('ne_' + x + '.ogg', x)
+
+audio('explode.ogg', 'explode')
+
+image('section1.png')
+
+for x in [0, 30, 45, 60, 90, 120, 135, 150, 180]:
+    image('line%s.png' % x, 'line%s' % x)
+
+image('protractor.png')
+image('section2ImgHint.png', 'section2ImgHint')
+image('angle.png', 'angle')
+image('gameOverSuccess.png', 'gameOverSuccess')
+image('gameOverTry.png', 'gameOverTry')
+image('medal.png')
+image('section3ImgTop.png', 'section3ImgTop')
+image('section3.png')
+image('ball.png')
+#image('check.png')
+image('boom.png', 'boom')
+
+audio('explode.ogg', 'explode')
