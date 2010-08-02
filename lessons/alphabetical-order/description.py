0a0da31de9186154748a1efe28b40af39d9d41e4
Grant Cox <grant.cox@gmail.com>
2010-08-02 14:07:29 +0545

    Updated Alphabetical Order lesson to new structure.  Should be updated soon to use new jQuery UI rather than ui.core - currently no snapping on drag & drop

---
+++ lessons/alphabetical-order/description.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+lesson(grade=6, subject='English', title='Alphabetical Order', week=13, browser_title='Class 6 English Alphabetical Order', lesson_title='Alphabetical Order')
+
+css('global')
+css('lesson.css')
+
+for f in [
+        'ui.core',
+        'ui.draggable',
+        'ui.droppable',
+        'jquery.clickable'
+    ]:
+        java_script(f)
+
+for index in range(0,6):
+    image('ele' + str(index) + '.png', 'ele' + str(index))
+    image('house' + str(index) + '.png', 'house' + str(index))
+    image('ship' + str(index) + '.png', 'ship' + str(index))
+
+audio('correct')
+audio('incorrect')
+
+image('correct.png', 'correct')
+image('incorrect.png', 'incorrect')
+image('background.jpg')
+
+footer_configuration(link_next=True, link_previous=True, scoreboard=True, link_check_answer=True)
