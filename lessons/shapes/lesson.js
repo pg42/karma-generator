1152ea20e4b9cecc949588e6f9cf8e28608e4b8a
Peter <Peter.Gijsels@gmail.com>
2010-08-05 12:29:36 +0545

    Refactored lesson shapes.

--- lessons/shapes/lesson.js
+++ lessons/shapes/lesson.js
@@ -1,197 +1,201 @@
-var shapes1 = ["quad1","quad2","quad3","quad4","circle1","circle2","circle3","circle4"];
-var shapes2 = ["quad21","quad22","quad23","quad24","circle21","circle22","circle23","circle24","triangle21","triangle22","triangle23","triangle24"];
-var shapes3 = ["triangle31","triangle32","triangle33","triangle34","quad31","quad31","quad32","quad33","quad34","quad34","circle31","circle31",
-               "circle32","circle32"];
-var dragged_object;
-var zIndex;
+var shapes1 = ['quad1','quad2','quad3','quad4','circle1','circle2','circle3','circle4'];
+var shapes2 = ['quad21','quad22','quad23','quad24','circle21','circle22','circle23','circle24','triangle21','triangle22','triangle23','triangle24'];
+var shapes3 = [
+    {
+        name: 'triangle31',
+        shape: 'triangle',
+        position: { left: 138, top: 65, width: 56, height: 36 }
+    },
+    {
+        name: 'triangle32',
+        shape: 'triangle',
+        position: { left: 410, top: 170, width: 53, height: 31 }
+    },
+    {
+        name: 'triangle33',
+        shape: 'triangle',
+        position: { left: 536, top: 177, width: 36, height: 34 }
+    },
+    {
+        name: 'triangle34',
+        shape: 'triangle',
+        position: { left: 658, top: 177, width: 49, height: 35 }
+    },
+    {
+        name: 'quad31',
+        shape: 'quad',
+        position: { left: 258, top: 125, width: 56, height: 32 }
+    },
+    {
+        name: 'quad31',
+        shape: 'quad',
+        position: { left: 364, top: 107, width: 56, height: 32 }
+    },
+    {
+        name: 'quad32',
+        shape: 'quad',
+        position: { left: 333, top: 172, width: 57, height: 33 }
+    },
+    {
+        name: 'quad33',
+        shape: 'quad',
+        position: { left: 338, top: 215, width: 53, height: 31 }
+    },
+    {
+        name: 'quad34',
+        shape: 'quad',
+        position: { left: 572, top: 180, width: 40, height: 30 }
+    },
+    {
+        name: 'quad34',
+        shape: 'quad',
+        position: { left: 616, top: 180, width: 40, height: 30 }
+    },
+    {
+        name: 'circle31',
+        shape: 'circle',
+        position: { left: 508, top: 240, width: 55, height: 35 }
+    },
+    {
+        name: 'circle31',
+        shape: 'circle',
+        position: { left: 733, top: 235, width: 55, height: 35 }
+    },
+    {
+        name: 'circle32',
+        shape: 'circle',
+        position: { left: 693, top: 107, width: 52, height: 42 }
+    },
+    {
+        name: 'circle32',
+        shape: 'circle',
+        position: { left: 788, top: 165, width: 52, height: 42 }
+    }
+];
+
+function handleDrop(karma, event, ui) {
+    var draggable = ui.draggable;
+    if (event.target.id == draggable.data('shape')) {
+        karma.play('correct');
+        draggable
+            .draggable({ disabled: true })
+            .css({ 'z-index': zIndex++ });
+    } else {
+        karma.play('incorrect');
+        draggable.animate(draggable.data('original_position'), 1000);
+    }
+}
 
 function createLesson (karma, content, shapes, lessonId) {
-    zIndex = 1;
+    var zIndex = 1;
     content
         .append(createDiv('section' + lessonId)
                 .append(createDiv('shapesSection' + lessonId))
-                .append(createDiv('dropSection' + lessonId)
-                        .append(createDiv('quad')
-                                .addClass('dropObjects')
-                                )
-                        .append(createDiv('circle')
-                                .addClass('dropObjects')
-                                )
-                        .append(createDiv()
-                                .html('चतुर्भुज')
-                                .addClass('dropText')
-                                )
-                        .append(createDiv()
-                                .html('वृत')
-                                .addClass('dropText')
-                                )
-                        )
-                );
-    if(lessonId == 2) {
+                .append(createDiv('dropSection' + lessonId)));
+
+    if (lessonId == 1) {
         $('#dropSection' + lessonId)
-            .empty()
+            .append(createDiv('quad')
+                    .addClass('dropObjects'))
+            .append(createDiv('circle')
+                    .addClass('dropObjects'))
+            .append(createDiv()
+                    .html('चतुर्भुज')
+                    .addClass('dropText'))
+            .append(createDiv()
+                    .html('वृत')
+                    .addClass('dropText'));
+    } else {
+        $('#dropSection' + lessonId)
             .append(createDiv('triangle')
-                        .addClass('dropObjects')
-                    )
-                    .append(createDiv('quad')
-                        .addClass('dropObjects')
-                    )
-                    .append(createDiv('circle')
-                        .addClass('dropObjects')
-                    )
-                    .append(createDiv()
-                        .html('त्रिभुज')
-                        .addClass('dropText')
-                    )
-                    .append(createDiv()
-                        .html('चतुर्भुज')
-                        .addClass('dropText')
-                    )
-                    .append(createDiv()
-                        .html('वृत')
-                        .addClass('dropText')
-                    );
+                    .addClass('dropObjects'))
+            .append(createDiv('quad')
+                    .addClass('dropObjects'))
+            .append(createDiv('circle')
+                    .addClass('dropObjects'))
+            .append(createDiv()
+                    .html('त्रिभुज')
+                    .addClass('dropText'))
+            .append(createDiv()
+                    .html('चतुर्भुज')
+                    .addClass('dropText'))
+            .append(createDiv()
+                    .html('वृत')
+                    .addClass('dropText'));
     }
-    var displayShape = function (img) {
-        $('#shapesSection' + lessonId)
-            .append(createDiv(img)
-                .addClass('dragObjects')
-                .append(Karma.createImg(img))
-                .data('shape', img.substring(0, img.length-lessonId))
-            );
-    };
-    
-    Karma.shuffle(shapes).forEach(displayShape);
+    Karma.shuffle(shapes)
+        .forEach(function (img) {
+                     $('#shapesSection' + lessonId)
+                         .append(createDiv(img)
+                                 .addClass('dragObjects')
+                                 .append(Karma.createImg(img))
+                                 .data('shape', img.substring(0, img.length-lessonId))
+                                 .data('original_position', { top: 0, left: 0 })
+                                );
+                 });
     $('.dragObjects')
-        .draggable({
-            revert: 'invalid',
-            start: function (event, ui) {
-                dragged_object = event.target.id;
-        }
-    });
+        .draggable({ revert: 'invalid' });
     $('.dropObjects')
         .droppable({
-            tolerence: 'intersect',
-            hoverClass: 'drophover',
-            drop : function (event, ui) {
-                if(event.target.id == $('#' + dragged_object).data('shape')) {
-                    karma.play('correct');
-                    $('#' + dragged_object)
-                        .draggable({disabled: true, revert: 'invalid'})
-                        .css({'z-index':zIndex});
-                        
-                    zIndex++;
-                }
-                else {
-                    karma.play('incorrect');
-                    $('#' + dragged_object)
-                        .draggable({revert: 'valid'})
-                }
-            }
-    });
+                       hoverClass: 'drophover',
+                       drop: function (event, ui) {
+                           handleDrop(karma, event, ui);
+                       }
+                   });
 }
 
 function lessonThree(karma, content) {
-    zIndex = 1;
+    var zIndex = 1;
     var img_count = 0;
     content
         .append(createDiv('section3')
-                    .append(createDiv('shapesSection3')
-                            .append(createDiv('shapes3Img'))
-                            )
-                    .append(createDiv('dropSection3')
-                            .append(createDiv('triangle')
-                                    .addClass('dropObjects3')
-                                    )
-                            .append(createDiv('quad')
-                                    .addClass('dropObjects3')
-                                    )
-                            .append(createDiv('circle')
-                                    .addClass('dropObjects3')
-                                    )
-                            )
+                .append(createDiv('shapesSection3')
+                        .append(createDiv('shapes3Img')))
+                .append(createDiv('dropSection3')
+                        .append(createDiv('triangle')
+                                .addClass('dropObjects3'))
+                        .append(createDiv('quad')
+                                .addClass('dropObjects3'))
+                        .append(createDiv('circle')
+                                .addClass('dropObjects3')))
+                .append(createDiv()
+                        .html('त्रिभुज')
+                        .addClass('dropText'))
+                .append(createDiv()
+                        .html('चतुर्भुज')
+                        .addClass('dropText'))
+                .append(createDiv()
+                        .html('वृत')
+                        .addClass('dropText')));
+
+    shapes3.forEach(function (shape) {
+                        $('#shapesSection3')
                             .append(createDiv()
-                                .html('त्रिभुज')
-                                .addClass('dropText')
-                            )
-                            .append(createDiv()
-                                .html('चतुर्भुज')
-                                .addClass('dropText')
-                            )
-                            .append(createDiv()
-                                .html('वृत')
-                                .addClass('dropText')
-                            )
-                );
-        
-    var gen_absolute_image = function (position) {
-        var dragObj = {
-                        'position':'absolute',                     
-                        'background-image':'url("assets/image/' + shapes3[img_count] + '.png")',
-                        'background-repeat': 'no-repeat'
-                        };
-        var identify = String(img_count).length<2?("0" + img_count):img_count;
-        $("#shapesSection3")
-            .append(createDiv(shapes3[img_count] + identify)
-                .addClass('dragObjects')
-                .data('shape', (shapes3[img_count]).substring(0, (shapes3[img_count]).length-2))
-        );
-        $('#' + shapes3[img_count] + identify)
-            .css(dragObj)
-            .css(position);
-        img_count++;
-    };
-    
-    var a = 6 ,b = -5;
-    gen_absolute_image({left: 132 + a, top: 70 + b, width: 56, height: 36});    // top triangle
-    gen_absolute_image({left: 404 + a, top: 175 + b, width: 53, height: 31});   // side tran
-    gen_absolute_image({left: 530 + a, top: 182 + b, width: 36, height: 34});   // glass left		
-    gen_absolute_image({left: 652 + a, top: 182 + b, width: 49, height: 35});   // glass middle right
-    gen_absolute_image({left: 252 + a, top: 130 + b, width: 56, height: 32});   // window1
-    gen_absolute_image({left: 358 + a, top: 112 + b, width: 56, height: 32});   // window 2
-    gen_absolute_image({left: 327 + a, top: 177 + b, width: 57, height: 33});   // door
-    gen_absolute_image({left: 332 + a, top: 220 + b, width: 53, height: 31});   // mat
-    gen_absolute_image({left: 566 + a, top: 185 + b, width: 40, height:30});   // glass middle 1
-    gen_absolute_image({left: 610 + a, top: 185 + b, width: 40, height: 30});   // glass middle 1
-    gen_absolute_image({left: 502 + a, top: 245 + b, width: 55, height: 35});   // tyre 1
-    gen_absolute_image({left: 727 + a, top: 240 + b, width: 55, height: 35});   // tyre 2
-    gen_absolute_image({left: 687 + a, top: 112 + b, width: 52, height: 42});   // fruit 1
-    gen_absolute_image({left: 782 + a, top: 170 + b, width: 52, height: 42});   // fruit 2
-    
-    
+                                    .addClass('dragObjects')
+                                    .data('shape', shape.shape)
+                                    .data('original_position', shape.position)
+                                    .css({ position: 'absolute' })
+                                    .css(shape.position)
+                                    .append(karma.createImg(shape.name)));
+                    });
+
     $('.dragObjects')
-        .draggable({
-            revert: 'invalid',
-            start: function (event, ui) {
-                dragged_object = event.target.id;
-        }
-    });
-    
+        .draggable({ revert: 'invalid' });
+
     $('.dropObjects3')
         .droppable({
-            tolerence: 'intersect',
-            hoverClass: 'drophover',
-            drop : function (event, ui) {
-                if(event.target.id == $('#' + dragged_object).data('shape')) {
-                    karma.play('correct');
-                    $('#' + dragged_object)
-                        .draggable({disabled: true, revert: 'invalid'})
-                        .css({'z-index':zIndex});
-                        
-                    zIndex++;
-                }
-                else {
-                    karma.play('incorrect');
-                    $('#' + dragged_object)
-                        .draggable({revert: 'valid'})
-                }
-            }
-    });
+                       hoverClass: 'drophover',
+                       drop: function (event, ui) {
+                           handleDrop(karma, event, ui);
+                       }
+                   });
 }
 
 setUpMultiScreenLesson(
     [
-     function (karma, content) { createLesson(karma, content, shapes1, 1); },
-     function (karma, content) { createLesson(karma, content, shapes2, 2); },
-     lessonThree]);
+        function (karma, content) { createLesson(karma, content, shapes1, 1); },
+        function (karma, content) { createLesson(karma, content, shapes2, 2); },
+        lessonThree
+    ]
+);
 
