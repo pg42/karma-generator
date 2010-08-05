db7005d4e96797df5ae935177a44b24f0e50ad4e
Peter <Peter.Gijsels@gmail.com>
2010-08-05 15:58:03 +0545

    Fixed a bug in the title of vocabulary-wild-animals.

--- lessons/vocabulary-wild-animals/description.py
+++ lessons/vocabulary-wild-animals/description.py
@@ -1,18 +1,18 @@
-lesson(grade=2, subject='English', title='Vocabulary Wild Animal', week=6)
+lesson(grade=2, subject='English', title='Vocabulary Wild Animals', week=6)
 
 include('../../templates/vocabulary/description.py')
 
-items = [ 		
-		dict(name='snake', left=245, top=32, width=145),
-		dict(name='rhino', left=150, top=290, width=135),
-		dict(name='elephant', left=525, top=310, width=210),
-		dict(name='bear', left=320, top=390, width=115),
-		dict(name='monkey', left=74, top=524, width=190),
-		dict(name='turtle', left=425, top=564, width=145),
-		dict(name='jackal', left=715, top=593, width=145),
-		dict(name='tiger', left=940, top=470, width=126)
-		]
-		
+items = [
+    dict(name='snake', left=245, top=32, width=145),
+    dict(name='rhino', left=150, top=290, width=135),
+    dict(name='elephant', left=525, top=310, width=210),
+    dict(name='bear', left=320, top=390, width=115),
+    dict(name='monkey', left=74, top=524, width=190),
+    dict(name='turtle', left=425, top=564, width=145),
+    dict(name='jackal', left=715, top=593, width=145),
+    dict(name='tiger', left=940, top=470, width=126)
+    ]
+
 register_objects(items)
 
 image('vocabImg.jpg')
