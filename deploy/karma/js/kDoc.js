245a780452357714ac34b2c7f8a9117534d44de9
Peter <Peter.Gijsels@gmail.com>
2010-08-06 17:36:20 +0545

    Cleaned up kDoc.js

--- deploy/karma/js/kDoc.js
+++ deploy/karma/js/kDoc.js
@@ -1,24 +1,15 @@
-$(document).ready(
-    function(){
+$(function () {
+      var back = 'index.html';
+      var doc = '';
+      var params = window.location.search.slice(1).split('&');
+      if (params) {
+          back = params[0].split('=')[1];
+          doc = params[1].split('=')[1];
+      }
 
-	var back = 'index.html';
+      $('#iframeLessonPlan').attr('src', '' + doc + '.html');
+      $('#kHeader').kHeader({ title: 'Teacher\'s Note', zoom: true });
+      $('#kHeaderBackBtn').click(function () { window.location = back; });
 
-	var parseUrlParams = function(){
-	    var doc = '';
-	    var params = window.location.search.slice(1).split('&');
-	    if (params){
-		back = params[0].split('=')[1];
-		doc = params[1].split('=')[1];
-	    }
-
-	    $('#iframeLessonPlan').attr('src', "" + doc + ".html");
-	};
-
-	var $kHeader = $('#kHeader').kHeader({title:"Teacher's Note", zoom: true});
-		
-	
-	$('#kHeaderBackBtn').click(function(){ window.location = back; });
-
-	parseUrlParams();
-	Karma.scaleToViewport();
-});
+      Karma.scaleToViewport();
+  });
