a8a8b5b6cbe512ceaa9ecfac0b242453717abac5
Peter <Peter.Gijsels@gmail.com>
2010-08-09 10:00:11 +0545

    Refactored description.py of lesson variables.

--- lessons/variables/description.py
+++ lessons/variables/description.py
@@ -6,7 +6,7 @@ css('global')
 css('lesson.css')
 
 for f in ['ui.core',
-		 'jquery.clickable']:
+          'jquery.clickable']:
     java_script(f)
 
 audio('wrong.ogg', 'incorrect')
@@ -18,23 +18,12 @@ image('bgMessage.png', 'bgMessage')
 image('bgQuestion.png', 'bgQuestion')
 image('bgQues.png', 'bgQues')
 image('gameOver.png', 'gameOver')
+image('check.png')
 
-image('img0.png', 'img0')
-image('img0Clicked.png', 'img0Clicked')
-image('img1.png', 'img1')
-image('img1Clicked.png', 'img1Clicked')
-image('img2.png', 'img2')
-image('img2Clicked.png', 'img2Clicked')
-image('img3.png', 'img3')
-image('img3Clicked.png', 'img3Clicked')
-image('img4.png', 'img4')
-image('img4Clicked.png', 'img4Clicked')
-image('img5.png', 'img5')
-image('img5Clicked.png', 'img5Clicked')
-image('img6.png', 'img6')
-image('img6Clicked.png', 'img6Clicked')
-image('img7.png', 'img7')
-image('img7Clicked.png', 'img7Clicked')
+
+for i in range(0, 8):
+    image('img%s.png' % i, 'img%s' % i)
+    image('img%sClicked.png' % i, 'img%sClicked' % i)
 
 footer_configuration(scoreboard=True, link_check_answer=True)
 
