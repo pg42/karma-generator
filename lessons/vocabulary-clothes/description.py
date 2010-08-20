a230780d0fdf9a405d61ce7ded50e7c41b1ffbe4
Peter <Peter.Gijsels@gmail.com>
2010-08-20 15:57:09 +0545

    Replaced suit by dress in vocabulary-clothes lesson (bug 846)

--- lessons/vocabulary-clothes/description.py
+++ lessons/vocabulary-clothes/description.py
@@ -7,7 +7,7 @@ items = [
     dict(name='cap', left=138, top=290, width=85),
     dict(name='shirt', left=387, top=285, width=110),
     dict(name='coat', left=650, top=287, width=100),
-    dict(name='suit', left=906, top=287, width=120),
+    dict(name='dress', left=906, top=287, width=120),
     dict(name='skirt', left=120, top=553, width=120),
     dict(name='gloves', left=372, top=553, width=134),
     dict(name='socks', left=639, top=553, width=120),
