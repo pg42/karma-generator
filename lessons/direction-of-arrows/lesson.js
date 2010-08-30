dcdf29169a5f47628c197c9568c9e76a77ca57bc
anup <anup.shakya@olenepal.org>
2010-08-30 10:48:52 +0545

    instruction changed #917

--- lessons/direction-of-arrows/lesson.js
+++ lessons/direction-of-arrows/lesson.js
@@ -1,7 +1,7 @@
 var arrows = range(1, 9).map(function (i) { return 'arrow' + i; });
 
 setUpSimpleMCQuiz(
-    { title: 'Identify the direction' },
+    { title: 'बायाँतिर भएको तीरसगँ मिल्ने तीरमा क्लिक गर :' },
     {
         presentOption: function (x) {
             return createDiv()
