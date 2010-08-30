d189be7ba03ca90884b65f2a8ad2cf8d9a94dc5e
Bidhan Adhikari <bidhan.adhikari@olenepal.org>
2010-08-30 16:53:18 +0545

    Add Vocabuary animals & birds activities on grade 3

---
+++ lessons/vocabulary-animals_for class 3/description.py
@@ -0,0 +1,22 @@
+﻿lesson(grade=3, subject='English', title='cANIMALS', week=4,
+    lesson_title='ANIMALS',
+    summary=u'केही जङ्गली जनावरहरूका नाम सिक्ने क्रियाकलाप')
+
+include('../../templates/vocabulary-with-game/vocabulary-with-game.py')
+
+animals = ['cheetah',
+           'crocodile',
+           'dinosaur',
+           'giraffe',
+           'kangaroo',
+           'octopus',
+           'panda',
+           'rhinoceros',
+           'whale',
+           'zebra']
+
+java_script('configuration.js')
+
+register_objects(animals)
+
+image('volImage.png')
---
+++ lessons/vocabulary-birds2_for Class3/description.py
@@ -0,0 +1,22 @@
+﻿lesson(grade=3, subject='English', title='VOCABULARY BIRDS', week=4,
+    lesson_title='Vocabulary: Birds',
+    summary=u'केही चराहरूका नाम सिक्ने क्रियाकलाप')
+
+include('../../templates/vocabulary-with-game/vocabulary-with-game.py')
+
+birds = ['bulbul',
+         'crane',
+         'egret',
+         'kingfisher',
+         'ostrich',
+         'penguin',
+         'swan',
+         'swift',
+         'vulture',
+         'woodpecker'];
+
+java_script('configuration.js')
+
+register_objects(birds)
+
+image('volImage.png')
